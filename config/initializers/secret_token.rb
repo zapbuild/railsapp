2e6e1e201a9bce1806b254c1feba1aa91a285f59
zapbuild <zapbuild@zapbuild-Unknow.(none)>
2013-02-11 12:39:21 +0530

    some message goes here

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrailsapp::Application.config.secret_token = 'd2d2630355439085dbb39d9f3472bfd83835e57e32673b011a18b8933f5fb816a92dc59332251e77dab13b226d830a6258c474e2a5d705e8faf104fee85464f4'
